08078fc321e6c939b9da2d049d7b464f7fdec8b9
zaiyangliu <lzyxn@126.com>
2018-10-03 21:31:19 +0800

    using recursive method

---
+++ binary-search/binsearch-recursion.c
@@ -0,0 +1,13 @@
+#define COMPARE(x, y) ( (x) < (y) ? -1 : ( (x) == (y) ) ? 0 : 1 )
+int binsearch(int list[], int searchnum, int left, int right){
+  int middle;
+  if(left <= right){
+    middle = (left + right) / 2;
+    switch (COMPARE(list[middle], searchnum) ) {
+    case -1: return binsearch(list, searchnum, middle + 1, right);
+    case 0: return middle;
+    case 1: return binsearch(list, searchnum, left, middle - 1);
+    }
+  }
+  return -1;
+}
