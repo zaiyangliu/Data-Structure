dfe260f1d79b0161fba0877949387dad3211ad37
zaiyangliu <lzyxn@126.com>
2018-10-04 10:39:30 +0800

    matrix

---
+++ matrix/print_matrix.c
@@ -0,0 +1,9 @@
+void print_matrix (int matrix[][MAX_SIZE], int rows, int cols){
+  int i, j;
+  for(i = 0; i < rows; i++){
+    for(j = 0; j < cols; j++){
+      printf("%d", matrix[i][j]);
+    }
+    printf("\n");
+  }
+}
