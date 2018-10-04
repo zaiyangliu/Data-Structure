d20520c98b72883eb1097b326d5f3087ddafdea8
zaiyangliu <lzyxn@126.com>
2018-10-04 12:03:50 +0800

    Update transpose.c

--- matrix/transpose.c
+++ matrix/transpose.c
@@ -1,7 +1,7 @@
 void transpose(int a[][MAX_SIZE]){
   int i, j, temp;
   for(i = 0; i < MAX_SIZE; i++){
-    for(j = i + 1; j < MAX_SIZE; j++){  //主要理解为啥从i+1开始；
+    for(j = i + 1; j < MAX_SIZE; j++){  //注意理解为啥从i+1开始；
       SWAP(a[i][j], a[j][i], temp);
     }
   }
